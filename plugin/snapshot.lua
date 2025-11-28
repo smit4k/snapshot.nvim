1ea3594ffc06407bb85adb926ff77e4579a382b3
smit4k <smit@smit.codes>
2025-11-28 02:13:11 -0500

    Add SnapshotVisual command to capture visual selection

--- plugin/snapshot.lua
+++ plugin/snapshot.lua
@@ -7,3 +7,7 @@ end, {})
 vim.api.nvim_create_user_command("SnapshotBuffer", function()
   print(snapshot.capture_buffer())
 end, {})
+
+vim.api.nvim_create_user_command("SnapshotVisual", function()
+  print(snapshot.capture_visual())
+end, { range = true })
