7b33dd44322444a1be447eded8f5901323fb90b7
Smit Patil <smit@smit.codes>
2026-02-10 23:59:21 -0500

    Remove test files from generator

--- generator/test_dirs.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("HOME env var: {:?}", std::env::var("HOME"));
-    println!("dirs::home_dir(): {:?}", dirs::home_dir());
-    println!("dirs::picture_dir(): {:?}", dirs::picture_dir());
-}
